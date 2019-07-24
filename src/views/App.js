8100ae9949f5997be880e585bd8a5114e2cdc4fa
foreo <foreo@ip-192-168-30-11.ap-northeast-1.compute.internal>
2019-07-24 13:42:13 +0800

    结构目录调整

---
+++ src/views/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../css/App.css';
+import Button from 'antd/es/button';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header"> 
+        <Button type="primary">Primary</Button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
